bd005e89be751c3e752c48698f98f21bf266bb94
Alyssia <115212411+alyssiapr@users.noreply.github.com>
2023-12-08 11:32:13 +0100

    Add files via upload

--- index.py
+++ index.py
@@ -28,40 +28,89 @@ EN_B.high()
 
 
 def avancer():
-        motor1_pin1.high()
-        motor1_pin2.low()
-        motor2_pin1.high()
-        motor2_pin2.low()
+    motor1_pin1.high()
+    motor1_pin2.low()
+    motor2_pin1.high()
+    motor2_pin2.low()
 
 def reculer():
     motor1_pin1.low()
     motor1_pin2.high()
     motor2_pin1.low()
     motor2_pin2.high()
-        
+
+def droite():
+    motor1_pin1.high()
+    motor1_pin2.high()
+    motor2_pin1.low()
+    motor2_pin2.low()
+    
+def gauche():
+    motor1_pin1.low()
+    motor1_pin2.low()
+    motor2_pin1.high()
+    motor2_pin2.high()
 
 while True:
     xValue = xAxis.read_u16()
     yValue = yAxis.read_u16()
     buttonValue = button.value()
-    
-    xStatus = "middle"
-    yStatus = "middle"
     buttonStatus = "not pressed"
-    """if xValue <= 600:
-        xStatus = "GAUCHE"
-    elif xValue >= 60000:
-        xStatus = "DROITE"
-    if yValue <= 600:
-        yStatus = "AVANCE"
-    elif yValue >= 60000:
-        yStatus = "RECULE"
     
-    print(" " + xStatus + " " + yStatus)"""
+    if buttonValue == 0:
+        buttonStatus = "pressed"
+        
+    if xValue <= 600:
+        xStatus = "left"
+        gauche()
+        print('gauche')
+        #time.sleep(0.2)
+        
+    if xValue >= 60000:
+        xStatus = "right"
+        droite()
+        print('droite')
+        #time.sleep(0.2)
+        
     if yValue <= 600:
-        print("a")
+        yStatus = "up"
         avancer()
-    time.sleep(0.1)
+        print('avancer')
+        #time.sleep(0.2)
+        
+    if yValue >= 60000:
+        yStatus = "down"
+        reculer()
+        print('recule')
+        #time.sleep(0.2)
+        
+
+    
+    
+    
+    
+    #if xValue <= 600:
+        #xStatus = "GAUCHE"
+    #elif xValue >= 60000:
+        #xStatus = "DROITE"
+    #if yValue <= 600:
+        #yStatus = avancer()
+    #elif yValue >= 60000:
+        #yStatus = "RECULE"
+        
+        
+        
+
+
+    #if yValue <= 600:
+       # print("a")
+        #avancer()
+   # time.sleep(0.1)
+   # if yValue <= 60000:
+    #    print("b")
+      #  gauche()
+       #reculer()
+   # time.sleep(0.1)
     
     
 #     FAIRE AVANCER LA VOITURE AVEC LA MANETTE
